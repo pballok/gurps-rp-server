173206a34025fbab681fd8fb962fae389a07f4b5
Peter Ballok <pballok@gmail.com>
2020-09-26 11:00:32 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+G.U.R.P.S. Remote Play Server
+
+G.U.R.P.S. stands for Generic Universal RolePlaying System and is owned by Steve Jackson Games. For more info see http://www.sjgames.com/gurps/
+
+GURPS RemotePlay is a project to allow GURPS players to play out battles remotely. It allows to create PC and NPC characters and helps with keeping track of battles.
+
+This is the server part of the project. For the client, see https://github.com/pballok/gurps-rp-client
---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	log.Printf("Received request, sending reply")
+
+	fmt.Fprint(w, "Hello World!")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
